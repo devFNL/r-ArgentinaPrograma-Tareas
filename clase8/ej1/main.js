2d37ea8f1b89465acf3a49e1e46d4c8e8da4bc04
Federico Lindberg <fedelindberg@gmail.com>
2023-10-06 19:51:23 -0300

    tareas 1 y 2 de clase 6

---
+++ clase8/ej1/main.js
@@ -0,0 +1,110 @@
+function validarNombre(nombre) {
+  const MAXIMO_CARACTERES = 50;
+
+  if (nombre.length === 0) {
+    return "El nombre debe tener al menos 1 caracter";
+  }
+  if (nombre.length > MAXIMO_CARACTERES) {
+    return "El nombre debe tener menos de 50 caracteres";
+  }
+  if (!/^[a-zA-Z]+$/i.test(nombre)) {
+    return "El nombre sólo debe contener letras";
+  }
+}
+
+function validarPais(pais) {
+  const $paises = document.querySelectorAll('[name="pais"] option');
+
+  let paisValido = false;
+
+  for (let i = 0; i < $paises.length; i++) {
+    if (pais === $paises[i].value && $paises[i].value !== "") {
+      paisValido = true;
+      break;
+    }
+  }
+
+  if (!paisValido) {
+    return "Ese país no existe en la lista";
+  }
+}
+
+function validarCarta(carta) {
+  if (carta.length === 0) {
+    return "Este campo debe tener al menos 1 caracter";
+  }
+  if (carta.length > 100) {
+    return "Este campo debe tener menos de 100 caracteres";
+  }
+  if (!/^[a-zA-Z0-9,\.-_ ]+$/i.test(carta)) {
+    return "Este campo sólo puede contener letras, números, puntos, comas y guiones";
+  }
+}
+
+function validarConducta() {
+  const conductaRadios = document.querySelectorAll('[name="conducta"]');
+  let seleccionado = false;
+
+  for (let i = 0; i < conductaRadios.length; i++) {
+    if (conductaRadios[i].checked) {
+      seleccionado = true;
+      break;
+    }
+  }
+
+  if (!seleccionado) {
+    return "Debes seleccionar tu conducta de este año";
+  }
+
+  return null;
+}
+
+function redireccionar() {
+  window.location.href = "wishlist.html";
+}
+
+function validarYEnviar(e) {
+  e.preventDefault();
+  const $formulario = document.querySelector("#form");
+  const nombre = $formulario.nombre.value;
+  const pais = $formulario.pais.value;
+  const carta = $formulario.regalos.value;
+  const conductaError = validarConducta();
+
+  const errores = [];
+  const nombreError = validarNombre(nombre);
+  if (nombreError) {
+    errores.push(nombreError);
+  }
+  const paisError = validarPais(pais);
+  if (paisError) {
+    errores.push(paisError);
+  }
+  const cartaError = validarCarta(carta);
+  if (cartaError) {
+    errores.push(cartaError);
+  }
+  if (conductaError) {
+    errores.push(conductaError);
+  }
+
+  const $errores = document.querySelector("#errores");
+  const $exito = document.querySelector("#exito");
+
+  if (errores.length > 0) {
+    $errores.innerHTML = "";
+    errores.forEach((error) => {
+      const li = document.createElement("li");
+      li.textContent = error;
+      $errores.appendChild(li);
+    });
+  } else {
+    $exito.style.display = "block";
+    $errores.innerHTML = "";
+    $formulario.style.display = "none";
+    setTimeout(redireccionar, 3000);
+  }
+}
+
+const $botonEnviar = document.querySelector("#botonEnviar");
+$botonEnviar.addEventListener("click", validarYEnviar);
--- clase8/ej2/ej2.js
+++ clase8/ej2/ej2.js
@@ -1,5 +1,6 @@
 const $botonMiembros = document.querySelector("#botonMiembros");
 const $formulario = document.querySelector("#formulario");
+const $items = document.querySelector("#items");
 const $cantMiembros = document.querySelector("#cantMiembros");
 const $edadMayor = document.querySelector("#edadMayor");
 const $edadMenor = document.querySelector("#edadMenor");
@@ -8,45 +9,113 @@ const $botonCalcular = document.querySelector("#botonCalcular");
 const $salarioMayor = document.querySelector("#salarioMayor");
 const $salarioMenor = document.querySelector("#salarioMenor");
 const $salarioPromedio = document.querySelector("#salarioPromedio");
+const $errores = document.querySelector("#errores");
+const $calculos = document.querySelector("#calculos");
+const MAXIMA_EDAD = 120;
+const MAXIMO_MIEMBROS = 30;
+
+// validaciones hechas:
+// edad: no puede estar vacia, no puede ser mayor a 120, y no puede tener decimales
+// salario: si esta vacio, cuenta como 0
+
+function validarEdad(edad) {
+  return !isNaN(edad) && edad >= 0 && edad < MAXIMA_EDAD;
+}
+
+function validarMiembros(cantMiembros) {
+  return (
+    !isNaN(cantMiembros) && cantMiembros > 0 && cantMiembros <= MAXIMO_MIEMBROS
+  );
+}
+
+function agregarError(error) {
+  const $errorItem = document.createElement("li");
+  $errorItem.textContent = error;
+  $errores.appendChild($errorItem);
+}
+
+function borrarErrores() {
+  while ($errores.firstChild) {
+    $errores.removeChild($errores.firstChild);
+  }
+}
 
 $botonMiembros.onclick = function () {
-  const cantMiembros = Number($cantMiembros.value);
+  borrarErrores();
 
-  for (let i = 0; i < cantMiembros; i++) {
+  const cantMiembros = Number($cantMiembros.value);
+  if (!validarMiembros(cantMiembros)) {
+    agregarError(
+      "Ingrese una cantidad válida de miembros. (mayor a 0 y menor que 30)"
+    );
+    return;
+  }
+
+  for (let i = 1; i <= cantMiembros; i++) {
     const $nuevaLabelEdad = document.createElement("label");
-    $nuevaLabelEdad.textContent = `Ingrese edad del ${i + 1}° integrante: `;
+    $nuevaLabelEdad.textContent = `Ingrese edad del ${i}° integrante: `;
 
     const $nuevoInputEdad = document.createElement("input");
     $nuevoInputEdad.type = "number";
-    $nuevoInputEdad.id = "edades";
+    $nuevoInputEdad.classList.add("edades");
+    $nuevoInputEdad.id = `edad${i}`;
 
     const $nuevaLabelSalario = document.createElement("label");
-    $nuevaLabelSalario.textContent = `Ingrese el salario mensual del ${
-      i + 1
-    }° integrante`;
+    $nuevaLabelSalario.textContent = `Ingrese el salario mensual del ${i}° integrante`;
 
     const $nuevoInputSalario = document.createElement("input");
     $nuevoInputSalario.type = "number";
-    $nuevoInputSalario.id = "salarios";
+    $nuevoInputSalario.classList.add("salarios");
+    $nuevoInputSalario.id = `salario${i}`;
 
     const $br = document.createElement("br");
 
-    $formulario.appendChild($nuevaLabelEdad);
-    $formulario.appendChild($nuevoInputEdad);
-    $formulario.appendChild($nuevaLabelSalario);
-    $formulario.appendChild($nuevoInputSalario);
-    $formulario.appendChild($br);
-  }
+    $items.appendChild($nuevaLabelEdad);
+    $items.appendChild($nuevoInputEdad);
+    $items.appendChild($nuevaLabelSalario);
+    $items.appendChild($nuevoInputSalario);
+    $items.appendChild($br);
 
-  return false;
+    $botonCalcular.style.display = "block";
+  }
 };
 
 $botonCalcular.onclick = function () {
-  const $edades = document.querySelectorAll("#edades");
+  borrarErrores();
 
+  const cantMiembros = Number($cantMiembros.value);
+  if (!validarMiembros(cantMiembros)) {
+    agregarError(
+      "Ingrese una cantidad válida de miembros. (mayor a 0 y menor que 30)"
+    );
+    return;
+  }
   const edades = [];
-  for (let i = 0; i < $edades.length; i++) {
-    edades.push(Number($edades[i].value));
+
+  for (let i = 1; i <= cantMiembros; i++) {
+    const edadInput = document.getElementById(`edad${i}`);
+    const edad = Number(edadInput.value);
+
+    if (edadInput.value.trim() === "") {
+      agregarError(`Debe ingresar la edad de todos los integrantes.`);
+      return;
+    }
+
+    if (!validarEdad(edad)) {
+      agregarError(
+        `La edad del ${i}° integrante no es válida. (mayor a 0 y menor que 120)`
+      );
+      return;
+    }
+
+    if (!Number.isInteger(edad) || edad <= 0 || edad >= MAXIMA_EDAD) {
+      agregarError(
+        `La edad del ${i}° integrante debe ser un número entero. (sin decimales)`
+      );
+      return;
+    }
+
+    edades.push(edad);
   }
 
   const edadMayor = Math.max(...edades);
@@ -58,23 +127,36 @@ $botonCalcular.onclick = function () {
   $edadMenor.textContent = `La edad menor es: ${edadMenor}`;
   $edadPromedio.textContent = `La edad promedio es: ${edadPromedio}`;
 
-  const $salarios = document.querySelectorAll("#salarios");
-
   const salarios = [];
   const MESES_EN_UN_ANIO = 12;
 
-  for (let i = 0; i < $salarios.length; i++) {
-    if ($salarios[i].value > 0) {
-      salarios.push(Number($salarios[i].value) * MESES_EN_UN_ANIO);
+  for (let i = 1; i <= cantMiembros; i++) {
+    const salarioInput = document.getElementById(`salario${i}`);
+    let salario = Number(salarioInput.value);
+
+    if (salarioInput.value.trim() === "") {
+      salario = 0;
+    }
+
+    if (salario > 0) {
+      salarios.push(salario * MESES_EN_UN_ANIO);
     }
   }
 
-  const salarioMayor = Math.max(...salarios);
-  const salarioMenor = Math.min(...salarios);
-  const salarioPromedio =
-    salarios.reduce((prev, curr) => prev + curr, 0) / salarios.length;
+  let salarioMayor = 0;
+  let salarioMenor = 0;
+  let salarioPromedio = 0;
+
+  if (salarios.length > 0) {
+    salarioMayor = Math.max(...salarios);
+    salarioMenor = Math.min(...salarios);
+    salarioPromedio =
+      salarios.reduce((prev, curr) => prev + curr, 0) / salarios.length;
+  }
 
   $salarioMayor.textContent = `El salario anual mayor es: $${salarioMayor}`;
   $salarioMenor.textContent = `El salario anual menor es: $${salarioMenor}`;
   $salarioPromedio.textContent = `El salario anual promedio es: $${salarioPromedio}`;
+
+  $calculos.style.display = "flex";
 };
